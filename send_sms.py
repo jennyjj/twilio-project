32208cbdcad5a7f5f58142994b4e6531e44f974b
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-18 20:27:05 -0700

    finished and sent the message

--- send_sms.py
+++ send_sms.py
@@ -5,20 +5,23 @@ from config import ACCOUNT_SID, AUTH_TOKEN, TWILIO_NUMBER
 def validates_choice(user_choice):
     """Returns bool of user's choice"""
 
-    pass
+    if user_choice == "yes":
+        return True
+    elif user_choice == "no":
+        return False
 
 
 def gets_to_number():
     """Ask user for phone number, returns it with +1 added"""
-
-    pass
-
+    number = raw_input("What is your number? ")
+    number = "+1" + number
+    return number
 
 def gets_message_to_send():
     """Ask user for message to send"""
 
-    pass
-
+    message = raw_input("What message would you like to send? ")
+    return message
 
 def send_message(to_number, to_message, ACCOUNT_SID,
                             AUTH_TOKEN, TWILIO_NUMBER):
@@ -38,7 +41,7 @@ def main(ACCOUNT_SID, AUTH_TOKEN, TWILIO_NUMBER):
 
     print "Welcome to this Twilio API Demo"
 
-    user_choice = raw_input("""Would you like to send a next message? """)
+    user_choice = (raw_input("Would you like to send a text message? ")).lower()
 
     validated_user_choice = validates_choice(user_choice)
 
@@ -50,7 +53,7 @@ def main(ACCOUNT_SID, AUTH_TOKEN, TWILIO_NUMBER):
         send_message(to_number, to_message, ACCOUNT_SID,
                             AUTH_TOKEN, TWILIO_NUMBER)
 
-        user_choice = raw_input("""Would you like to send another next message? """)
+        user_choice = raw_input("Would you like to send another next message? ")
 
         validated_user_choice = validates_choice(user_choice)
 
